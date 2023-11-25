6b3c4b17dcf159d416dbb9d9da80960e2ac0b1e7
Syed M Muslim <117216018+Syed-Muslim-19@users.noreply.github.com>
2023-11-26 02:11:18 +0500

    Add files via upload

---
+++ LoginPage/script.js
@@ -0,0 +1,29 @@
+function View() {
+  var password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
+
+function Verify() {
+  var e = document.getElementById("email");
+  var p = document.getElementById("password");
+  var Email = e.value;
+  var Password = p.value;
+
+  if (Email === "admin" && Password === "admin") {
+    var forms = document.getElementsByClassName("Form");
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].style.display = "none";
+    }
+
+    var lsElements = document.getElementsByClassName("LS");
+    for (var j = 0; j < lsElements.length; j++) {
+      lsElements[j].style.display = "block";
+    }
+  } else {
+    alert("Invalid Email or Password");
+  }
+}
